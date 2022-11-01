c4cf24c7f0240e20488558eed65c3ad7a6b8fda6
majusoler <juliasolernunes@ymail.com>
2022-11-01 11:26:50 +0100

    Use State hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState } from "react";
+
+// Focus of this lecture
+// 1- Understand that hooks are? Functions done by someone else.
+// 2- Use useState ( First hook of the day)
 
 function App() {
+  // let number = 0;
+
+  const [number, setNumber] = useState(0);
+  const [favorite, setFavorite] = useState(false);
+
+  //Use state is a hook that has 2 itens:
+  // The first = A const that access the value
+  // The second = A setter Functions that changes the value of the const.
+
+  const increaseCounter = () => {
+    // number++;
+    // return number;
+    setNumber(number + 1);
+    // console.log(`this is my number: ${number}`);
+  };
+
+  const changeFavorite = () => {
+    //Option 1
+    setFavorite(favorite ? false : true);
+    //Option 2
+    // setFavorite(!favorite);
+  };
+
+  console.log(favorite);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is my app
+      <div>Number {number} of Likes</div>
+      <button onClick={increaseCounter}>Like</button>
+      <div>
+        {favorite ? <span>I like it</span> : <span>I don't like it</span>}
+      </div>
+      <button onClick={setFavorite(!favorite)}>Clicke me!</button>
     </div>
   );
 }
